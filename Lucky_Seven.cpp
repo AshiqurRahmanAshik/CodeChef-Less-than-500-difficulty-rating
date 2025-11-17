623b5b34a9b9f12cea19c693ad4aab2025b04704
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-17 22:54:52 +0600

    Lucky Seven

---
+++ Lucky_Seven.cpp
@@ -0,0 +1,14 @@
+/* 
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LUCKYSEVEN
+ Date:  17/11/25 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string luckyLetter;
+    cin >> luckyLetter;
+    cout << luckyLetter[6];
+    return 0;
+}
