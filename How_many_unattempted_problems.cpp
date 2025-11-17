6c42b95ebfc9c8e3c6aa65bed4289182e44b9d97
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-17 23:51:40 +0600

    How Many Unattempted Problems

---
+++ How_many_unattempted_problems.cpp
@@ -0,0 +1,14 @@
+/* 
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PRACLIST
+ Date: 17/11/25  
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int problems, attempts;
+    cin >> problems >> attempts;
+    cout << problems-attempts;
+    return 0;
+}
