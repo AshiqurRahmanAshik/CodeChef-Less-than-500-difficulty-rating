b1a96066553671549b09b4e2af2389dc3371b69f
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-17 23:09:57 +0600

    Double Rent

---
+++ Double_Rent.cpp
@@ -0,0 +1,14 @@
+/* 
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DOUBLERENT
+ Date:   17/11/25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int rent;
+    cin >> rent;
+    cout << 2*rent;
+    return 0;
+}
