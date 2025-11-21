0d684fec4551ca08dccab65b9ab667eb94524c07
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-22 03:43:53 +0600

    Who is taller

---
+++ Who_is_taller.cpp
@@ -0,0 +1,21 @@
+/* 
+Problem Link: 
+ Date:   
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tc, x, y;
+    cin >> tc;
+    while (tc--){
+         cin >> x >> y;
+    if (x > y){
+        cout << "A" << endl;
+    }else {
+        cout << "B" << endl;
+    }
+    }
+    return 0;
+}
