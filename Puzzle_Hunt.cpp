ac2598a6d4b92ca8de94292874639e48f68028f5
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-22 03:22:33 +0600

    Puzzle Hunt

---
+++ Puzzle_Hunt.cpp
@@ -0,0 +1,18 @@
+/* 
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PUZHUNT
+ Date: 22/11/25  
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int participants;
+    cin >> participants;
+    if (participants>=6 && participants <=8){
+        cout <<"Yes";
+    }else{
+        cout <<"No";
+    }
+    return 0;
+}
