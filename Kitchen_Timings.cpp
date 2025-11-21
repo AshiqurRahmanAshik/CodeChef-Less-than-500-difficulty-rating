47dbf02683e7369aa3aafab18c0a478f064dcffe
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-22 03:03:02 +0600

    Kitchen Timing

---
+++ Kitchen_Timings.cpp
@@ -0,0 +1,18 @@
+/* 
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/KITCHENTIME
+ Date: 27/11/ 25 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tc,x,y;
+    cin >> tc;
+    while(tc--){
+    cin >> x >>y;
+    cout << y-x << endl;    
+    }
+
+    return 0;
+}
