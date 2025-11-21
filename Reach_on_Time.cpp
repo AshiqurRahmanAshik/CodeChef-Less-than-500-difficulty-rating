710295c5a9d2098f99dd003ad16de07b97eb6098
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-22 03:15:31 +0600

    Reach on time

---
+++ Reach_on_Time.cpp
@@ -0,0 +1,22 @@
+/* 
+Problem Link: 
+ Date:   
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tc, time;
+    cin >> tc;
+    while (tc--){
+        cin >> time;
+        if (time >=30){
+            cout <<"YES" <<endl;
+        }else{
+            cout <<"NO" <<endl;
+
+        }
+    }
+    return 0;
+}
