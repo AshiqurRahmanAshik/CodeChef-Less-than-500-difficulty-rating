779084e8d01d79c25a8d90a46c500c03927e9910
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-22 02:57:23 +0600

    Off by One

---
+++ Off_By_One.cpp
@@ -0,0 +1,14 @@
+/* 
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OFFBY1
+ Date: 22/11/25  
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cin >> num1 >> num2;
+    cout << num1+num2 <<"1";
+    return 0;
+}
