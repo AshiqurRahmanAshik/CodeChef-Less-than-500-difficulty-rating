6e5505af0118140607dc781b391dbfabb99db641
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-22 02:55:13 +0600

    404 Not Found

---
+++ 404_Not_Found.cpp
@@ -0,0 +1,19 @@
+/* 
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ERROR404
+ Date: 22/11/25  
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int code;
+    cin >> code;
+    if (code==404){
+        cout << "NOT FOUND";
+    }else{
+        cout << "FOUND";
+
+    }
+    return 0;
+}
