f7328c2e231d2d83709d26ec6bd24ed0814a49f0
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-22 09:20:06 +0600

    2000

---
+++ 2000.cpp
@@ -0,0 +1,14 @@
+/* 
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RIP2000
+ Date: 22/11/25  
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << 4*n;
+    return 0;
+}
