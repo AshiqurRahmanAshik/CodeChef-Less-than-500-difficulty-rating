15496a995b7a2c0b3ee2563a5232bd400755d5fc
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-22 09:56:14 +0600

    Counting Words

---
+++ Battery_Health.cpp
@@ -0,0 +1,12 @@
+/* 
+Problem Link: 
+ Date:   
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
---
+++ Counting_Words.cpp
@@ -0,0 +1,17 @@
+/* 
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD
+ Date: 22/11/25   
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tc, x,y;
+    cin >> tc;
+    while(tc--){
+        cin >> x >> y;
+        cout << x * y <<endl;
+    }
+    return 0;
+}
