a758a4609c231c14af9906a2a5e8a78f8037358c
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-22 09:23:23 +0600

    Roller Coaster

---
+++ Roller_Coaster.cpp
@@ -0,0 +1,22 @@
+/* 
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MINHEIGHT
+ Date: 22/11/25  
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tc , x ,y;
+    cin >> tc;
+    while (tc--){
+        cin >> x >> y;
+        if (y>x){
+            cout << "NO"<<endl;
+        }else{
+            cout << "YES"<<endl;
+
+        }
+    }
+    return 0;
+}
