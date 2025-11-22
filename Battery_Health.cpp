fff09405921b9866414fdb7034a6eb0bccd855f7
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-22 09:59:31 +0600

    Battery Health

--- Battery_Health.cpp
+++ Battery_Health.cpp
@@ -1,12 +1,21 @@
 /* 
-Problem Link: 
- Date:   
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BTRYHLTH
+ Date: 22/11/25  
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    
+    int tc,x;
+    cin >> tc ;
+    while(tc--){
+        cin >> x;
+        if (x>=80){
+            cout <<"YES" <<endl;
+        }else{
+            cout <<"NO"<<endl;
+        }
+    }
     return 0;
 }
