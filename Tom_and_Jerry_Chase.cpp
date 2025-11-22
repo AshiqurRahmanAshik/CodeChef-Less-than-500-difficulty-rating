adee8cbfe37bddf25e9acbabcc23a4d7cd9f24c1
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-22 11:08:45 +0600

    Tom and Jerry Chase

---
+++ Tom_and_Jerry_Chase.cpp
@@ -0,0 +1,21 @@
+/* 
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/JERRYCHASE
+ Date: 22/11/25   
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tc, x , y;
+    cin >> tc;
+    while(tc--){
+    cin >> x >> y;
+    if (x<y){
+    cout << "YES" <<endl;
+    }else{
+    cout << "NO" <<endl;
+    }
+    }
+    return 0;
+}
