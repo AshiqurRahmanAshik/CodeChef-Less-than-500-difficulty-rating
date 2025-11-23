e53b6d5460c4700c1d69d8e831429674e62dd41b
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-23 10:06:16 +0600

    Sum it

---
+++ Chef_and_Donation.cpp
@@ -0,0 +1,19 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DNATION
+ Date: 23/11/25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc, x, y;
+    cin >> tc;
+    while (tc--)
+    {
+        cin >> x >> y;
+        cout << y - x << endl;
+    }
+    return 0;
+}
