5627c2d2c2d59136eb6785ce628d9ee928642290
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-23 11:15:40 +0600

    Mana Points

---
+++ Mana_Points.cpp
@@ -0,0 +1,19 @@
+/*
+Problem Link:
+ Date:
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc, x, y;
+    cin >> tc;
+    while (tc--)
+    {
+        cin >> x >> y;
+        cout << y / x << endl;
+    }
+    return 0;
+}
