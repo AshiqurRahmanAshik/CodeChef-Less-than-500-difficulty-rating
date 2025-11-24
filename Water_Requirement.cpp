a8b06068f8d90d020aa085c21c379c82dcde3112
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-24 11:32:15 +0600

    water requirement

---
+++ Water_Requirement.cpp
@@ -0,0 +1,19 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WATERREQ
+ Date: 24/11/25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc, x;
+    cin >> tc;
+    while (tc--)
+    {
+        cin >> x;
+        cout << 2 * x << endl;
+    }
+    return 0;
+}
