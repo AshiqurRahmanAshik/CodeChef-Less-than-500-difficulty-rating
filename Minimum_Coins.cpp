1ba77f807e280ac57657b68c0833796bd6070b52
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-26 09:57:45 +0600

    Minimum Coins

---
+++ Minimum_Coins.cpp
@@ -0,0 +1,19 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MINCOINSREQ
+ Date: 26/11/25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc, x;
+    cin >> tc;
+    while (tc--)
+    {
+        cin >> x;
+        cout << x % 10 << endl;
+    }
+    return 0;
+}
