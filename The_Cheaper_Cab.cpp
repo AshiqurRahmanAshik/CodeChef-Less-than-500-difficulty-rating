420d46debabf94947d70a6b6bb41b07e25ea30a2
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-26 10:28:08 +0600

    The cheaper cab

---
+++ The_Cheaper_Cab.cpp
@@ -0,0 +1,31 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CABS
+ Date:   26/11/25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc, x, y;
+    cin >> tc;
+    while (tc--)
+    {
+        cin >> x >> y;
+        if (x < y)
+        {
+            cout << "FIRST" << endl;
+        }
+        else if (y < x)
+        {
+
+            cout << "SECOND" << endl;
+        }
+        else
+        {
+            cout << "ANY" << endl;
+        }
+    }
+    return 0;
+}
