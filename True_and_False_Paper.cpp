541c5e907c313e86f2b52663334d68e141fde9d7
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-26 10:24:55 +0600

    True and False Paper

---
+++ True_and_False_Paper.cpp
@@ -0,0 +1,20 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TFPAPER
+ Date: 26/11/25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc, n, k;
+    cin >> tc;
+    while (tc--)
+    {
+        cin >> n >> k;
+        cout << n - k << endl;
+    }
+
+    return 0;
+}
