2ff02efffc9c777972ab54478b7998326f488ae8
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-26 10:31:08 +0600

    Discount

---
+++ Discount.cpp
@@ -0,0 +1,19 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DISCNT
+ Date: 26/11/25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc, x;
+    cin >> tc;
+    while (tc--)
+    {
+        cin >> x;
+        cout << 100 - x << endl;
+    }
+    return 0;
+}
