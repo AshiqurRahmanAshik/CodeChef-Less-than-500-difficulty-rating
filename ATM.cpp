0759fa65abda017698aa6bad7b47e33405bc16c0
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-26 11:01:17 +0600

    ATM

---
+++ ATM.cpp
@@ -0,0 +1,23 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/HS08TEST
+ Date:   26/11/25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float x, y;
+    cin >> x >> y;
+    if (int(x) % 5 != 0 || y < x+0.5) // modulo operator can not operate on float value
+    {
+        cout << fixed << setprecision(2) << y;
+    }
+    else
+    {
+        cout << fixed << setprecision(2) << y - x - 0.5;
+    }
+
+    return 0;
+}
