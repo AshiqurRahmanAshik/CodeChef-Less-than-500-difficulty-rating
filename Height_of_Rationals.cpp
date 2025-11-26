1d4007d8fc9a6fbc1146dd29d9626826395d0f53
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-26 10:34:44 +0600

    Height of Retionals

---
+++ Height_of_Rationals.cpp
@@ -0,0 +1,22 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/HEIGHTRATION
+ Date: 26/11/25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    if (x > y)
+    {
+        cout << x;
+    }
+    else
+    {
+        cout << y;
+    }
+    return 0;
+}
