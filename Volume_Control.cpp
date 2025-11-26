8306014be879bc4560dc9fff84199b58af649bf8
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-26 10:42:11 +0600

    Volume Control

---
+++ Volume_Control.cpp
@@ -0,0 +1,26 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/VOLCONTROL
+ Date: 26/11/25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc, x, y;
+    cin >> tc;
+    while (tc--)
+    {
+        cin >> x >> y;
+        if (x > y)
+        {
+            cout << x - y << endl;
+        }
+        else
+        {
+            cout << y - x << endl;
+        }
+    }
+    return 0;
+}
