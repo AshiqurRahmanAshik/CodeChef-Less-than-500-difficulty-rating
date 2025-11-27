fd8484ab13fb3951cec22a9ae249ca5eb0ffc8f5
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-27 10:48:27 +0600

    Ezio and Guards

---
+++ Ezio_and_Guards.cpp
@@ -0,0 +1,26 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MANIPULATE
+ Date: 27/11/25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc, x, y;
+    cin >> tc;
+    while (tc--)
+    {
+        cin >> x >> y;
+        if (x >= y)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
