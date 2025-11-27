8fb96ae51a28d56aa83dde43cb57c20ca245c6c9
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-27 11:15:18 +0600

    Maximum Submissions

---
+++ Maximum_Submissions.cpp
@@ -0,0 +1,19 @@
+/*
+Problem Link:
+ Date:
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc, min;
+    cin >> tc;
+    while (tc--)
+    {
+        cin >> min;
+        cout << (min * 60) / 30 << endl;
+    }
+    return 0;
+}
