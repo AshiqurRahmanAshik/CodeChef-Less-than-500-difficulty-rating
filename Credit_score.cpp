26af3b198853c61a524fc7741151a594511b71da
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-28 00:45:56 +0600

    Credit Score

---
+++ Credit_score.cpp
@@ -0,0 +1,23 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CREDSCORE
+ Date: 28/11/25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    if (x >= 750)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
