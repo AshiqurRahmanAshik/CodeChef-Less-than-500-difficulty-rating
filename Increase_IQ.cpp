b9b37494248838591b61f293d8fbdf4aca130055
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-12-01 11:03:51 +0600

    Increase IQ

---
+++ Increase_IQ.cpp
@@ -0,0 +1,22 @@
+/*
+Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/INCRIQ
+ Date: 01/11/25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    if (x >= 163)
+    {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+    return 0;
+}
