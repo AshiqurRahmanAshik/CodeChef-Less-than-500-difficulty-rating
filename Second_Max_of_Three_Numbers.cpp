418158838e9f8ae610f076fc197482f5f46ca38d
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-12-01 12:22:25 +0600

    Second Max

--- Second_Max_of_Three_Numbers.cpp
+++ Second_Max_of_Three_Numbers.cpp
@@ -1,25 +1,22 @@
-/* 
+/*
 Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SNDMAX
- Date: 22/11/25  
+ Date: 22/11/25
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int tc, x,y,z;
+int main()
+{
+    int tc, a, b, c;
     cin >> tc;
-    while (tc--){
-    cin >> x >> y >> z;
-    if ( y > x && y <z){
-        cout << y <<endl;
-    }
-    else if ( x < y && x <z){
-        cout << x <<endl;
-    }
-    else if( z > x && z < y ){
-        cout << z <<endl;
-    }
+    while (tc--)
+    {
+        cin >> a >> b >> c;
+        int mx = max({a, b, c});
+        int mn = min({a, b, c});
+        int second_max = a + b + c - mx - mn;
+        cout << second_max << endl;
     }
     return 0;
 }
